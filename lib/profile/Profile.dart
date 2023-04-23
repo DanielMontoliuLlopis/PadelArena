63f0fe7f6cdd6fdf8c957865db5ff2362eee52d9
Daniel Montoliu <danllo@alu.iesserpis.org>
2023-04-23 22:46:02 +0200

    feat(Profile Page without edit option)

--- lib/profile/Profile.dart
+++ lib/profile/Profile.dart
@@ -7,65 +7,152 @@ import 'package:get/get.dart';
 
 import '../HomePage/HomePage.dart';
 
-class Profile extends StatefulWidget{
+class Profile extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-      return _ProfileState();
+    return _ProfileState();
   }
-
 }
-class _ProfileState extends State<Profile>{
 
+class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-      return(
-        Column(children:[
-           Text("${userData?.userName}"),
-           Text("${userData?.email}"),
-           Text("${userData?.firstName}"),
-           Text("${userData?.lastName}"),
-          InkWell(
-                onTap: () {
-                  logOut();
-                },
-                child: Container(
-                  margin: const EdgeInsets.only(bottom: 30),
-                  height: 70,
-                  width: Get.width,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
+    return (Column(
+      children: [
+        Container(
+          height: 200,
+        ),
+        Container(
+          width: Get.width,
+          height: Get.height * 0.075,
+          decoration: BoxDecoration(
+             boxShadow: [
+                BoxShadow(
+                  color: Colors.greenAccent[200]!,
+                  offset: const Offset(
+                    5.0,
+                    5.0,
                   ),
-                  child: const Center(
-                    child: Text(
-                      "Log Out",
-                      style: TextStyle(
-                        fontFamily: "HappyMonkey",
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                ),
+                const BoxShadow(
+                  color: Colors.white,
+                  offset:  Offset(0.0, 0.0),
+                  blurRadius: 0.0,
+                  spreadRadius: 0.0,
+                ),
+              ],
+              border: Border.all(
+            width: 2,
+          )),
+          child: Center(child: Text("User: ${userData?.userName}")),
+        ),
+        Container(
+          height: 50,
+        ),
+        Container(
+          width: Get.width,
+          height: Get.height * 0.075,
+          decoration: BoxDecoration(
+             boxShadow: [
+                BoxShadow(
+                  color: Colors.greenAccent[200]!,
+                  offset: const Offset(
+                    5.0,
+                    5.0,
                   ),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                ),
+                const BoxShadow(
+                  color: Colors.white,
+                  offset:  Offset(0.0, 0.0),
+                  blurRadius: 0.0,
+                  spreadRadius: 0.0,
+                ),
+              ],
+              border: Border.all(
+            width: 2,
+          )),
+          child: Center(child: Text("Email: ${userData?.email}")),
+        ),
+        Container(
+          height: 50,
+        ),
+        Container(
+          width: Get.width,
+          height: Get.height * 0.075,
+          decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.greenAccent[200]!,
+                  offset: const Offset(
+                    5.0,
+                    5.0,
+                  ),
+                  blurRadius: 10.0,
+                  spreadRadius: 2.0,
+                ),
+                const BoxShadow(
+                  color: Colors.white,
+                  offset:  Offset(0.0, 0.0),
+                  blurRadius: 0.0,
+                  spreadRadius: 0.0,
+                ),
+              ],
+              border: Border.all(
+                width: 2,
+              )),
+          child: Center(
+              child:
+                  Text("Nombre: ${userData?.firstName} ${userData?.lastName}")),
+        ),
+        Container(
+          height: 100,
+        ),
+        InkWell(
+          onTap: () {
+            logOut();
+          },
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 30),
+            height: 70,
+            width: Get.width,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Center(
+              child: Text(
+                "Log Out",
+                style: TextStyle(
+                  fontFamily: "HappyMonkey",
+                  color: Colors.white,
+                  fontSize: 18,
                 ),
               ),
-        ],)
-      );
+            ),
+          ),
+        ),
+      ],
+    ));
   }
 
-  Future logOut() async{
-    showDialog(context: context, builder: (context)=>
-      const Center(child: CircularProgressIndicator()), 
+  Future logOut() async {
+    showDialog(
+      context: context,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
       barrierDismissible: false,
     );
     try {
       await FirebaseAuth.instance.signOut();
-      userData=null;
-      email=null;
-      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>  LogIn() ), ((route) => false));
-    } on FirebaseAuthException catch  (e) {
-        navigatorKey.currentState!.popUntil((route) => route.isFirst);
+      userData = null;
+      email = null;
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => LogIn()), ((route) => false));
+    } on FirebaseAuthException catch (e) {
+      navigatorKey.currentState!.popUntil((route) => route.isFirst);
     }
-    
   }
-
 }
