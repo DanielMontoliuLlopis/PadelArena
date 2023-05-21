a4703fb197dd997a23fc539a9817d46e5816d808
Daniel Montoliu <danllo@alu.iesserpis.org>
2023-05-21 23:27:51 +0200

    Block orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:padel_arena/Model/TournamentData.dart';
@@ -17,7 +18,13 @@ void main() async {
   await Future.delayed(const Duration(seconds: 2));
   FlutterNativeSplash.remove();
 
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized(); 
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]) 
+  .then((_) { 
+    runApp( MyApp()); 
+    }
+  );
+
 }
 
 String? email = "";
